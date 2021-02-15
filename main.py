d2a3d39954cf8c68ab1e1aa0f8430e78a8df52ae
tianyliu <tianyu.liu-1@student.uts.edu.au>
2021-02-16 02:23:23 +1100

    data processing

--- main.py
+++ main.py
@@ -10,6 +10,27 @@ import torch.utils.data.dataset as Dataset
 # 引入DataLoader：
 import torch.utils.data.dataloader as DataLoader
 
+
+
+
+    # 创建子类
+class subDataset(Dataset.Dataset):
+    # 初始化，定义数据内容和标签
+    def __init__(self, Data, Label):
+        self.Data = Data
+        self.Label = Label
+
+    # 返回数据集大小
+    def __len__(self):
+        return len(self.Data)
+
+    # 得到数据内容和标签
+    def __getitem__(self, index):
+        data = torch.Tensor(self.Data[index])
+        label = torch.IntTensor(self.Label[index])
+        return data, label
+
+
 def main():
 
     Data_Vector_Length = 200
@@ -36,7 +57,33 @@ def main():
     Drift_train_x, Drift_train_y = torch.tensor(train_drift_data).split(Data_Vector_Length, 1)
     Drift_test_x, Drift_test_y = torch.tensor(test_drift_data).split(Data_Vector_Length, 1)
 
-    # Drift_data, Drift_label = Drift_dat_tensor.split(49, 1)
+    Drift_train_x, Drift_train_y = Drift_train_x.numpy(), Drift_train_y.numpy()
+    Drift_test_x, Drift_test_y = Drift_test_x.numpy(), Drift_test_y.numpy()
+
+
+
+    # Drift_train_x, Drift_train_y = Drift_train_x.numpy(), Drift_train_y.numpy()
+    # Drift_test_x, Drift_test_y = Drift_test_x.numpy(), Drift_test_y.numpy()
+    train_dataset = subDataset(Drift_train_x, Drift_train_y)
+    test_dataset = subDataset(Drift_test_x, Drift_test_y)
+
+
+    # 创建DataLoader迭代器
+    # 创建DataLoader，batch_size设置为2，shuffle=False不打乱数据顺序，num_workers= 4使用4个子进程：
+    train_dataloader = DataLoader.DataLoader(train_dataset, batch_size=20, shuffle=False, num_workers=4)
+    test_dataloader = DataLoader.DataLoader(test_dataset, batch_size=20, shuffle=False, num_workers=4)
+    # 使用enumerate访问可遍历的数组对象：
+    for i, item in enumerate(train_dataloader):
+        print('i:', i)
+        data, label = item
+        print('data:', data)
+        print('label:', label)
+
+
+
+
+
+
 
     print('Checking if GPU is available')
     use_gpu = torch.cuda.is_available()
